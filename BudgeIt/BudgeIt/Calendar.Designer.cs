d46b76255d9694e1c1ca00ff183b07982511f35b
Nathan Hudson <nathandhudson@gmail.com>
2020-02-18 14:02:31 -0600

    SqlConnection Test

--- BudgeIt/BudgeIt/Calendar.Designer.cs
+++ BudgeIt/BudgeIt/Calendar.Designer.cs
@@ -29,25 +29,36 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.RTBTesting = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(423, 266);
+            this.label1.Location = new System.Drawing.Point(30, 20);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(148, 25);
             this.label1.TabIndex = 0;
             this.label1.Text = "Good Morning";
+            // 
+            // RTBTesting
+            // 
+            this.RTBTesting.Location = new System.Drawing.Point(424, 252);
+            this.RTBTesting.Name = "RTBTesting";
+            this.RTBTesting.Size = new System.Drawing.Size(819, 509);
+            this.RTBTesting.TabIndex = 1;
+            this.RTBTesting.Text = "";
             // 
             // Calendar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1647, 1003);
+            this.Controls.Add(this.RTBTesting);
             this.Controls.Add(this.label1);
             this.Name = "Calendar";
             this.Text = "Calendar";
+            this.Load += new System.EventHandler(this.Calendar_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -56,6 +67,7 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox RTBTesting;
     }
 }
 
--- BudgeIt/BudgeIt/Calendar.cs
+++ BudgeIt/BudgeIt/Calendar.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,46 @@ namespace BudgeIt
 {
     public partial class Calendar : Form
     {
+        SqlConnection sqlConnection = new SqlConnection();
+
         public Calendar()
         {
             InitializeComponent();
+        }
+
+        private void Calendar_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                /*sqlConnection.ConnectionString =
+                    "Data Source=SERVERNAME;" + // Change to your server name ------------
+                    "Initial Catalog=BudgeIt;" +
+                    "Integrated Security=True";
+                sqlConnection.Open();*/
+
+                sqlConnection.ConnectionString =
+                    "Data Source=NATE-SURFACE;" +
+                    "Initial Catalog=BudgeIt;" +
+                    "Integrated Security=True";
+                sqlConnection.Open();
+
+                MessageBox.Show("Connected to DB");
+
+                SqlCommand cmdTest =  sqlConnection.CreateCommand();
+                cmdTest.CommandText = "SELECT Fname , Lname FROM USERS ";
+
+                SqlDataReader reader = cmdTest.ExecuteReader();
+
+                if(reader.Read())
+                {
+                        RTBTesting.Text = RTBTesting.Text + reader[0].ToString().Trim() + " " + reader[1].ToString().Trim();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
