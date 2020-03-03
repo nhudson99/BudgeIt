de7388b9524b921047c9b68a16acbbe90bc284a0
justin garcia <justin.garcia953@gmail.com>
2020-03-03 15:40:29 -0600

    remade table

--- BudgeIt/BudgeIt/Calendar.cs
+++ BudgeIt/BudgeIt/Calendar.cs
@@ -34,10 +34,7 @@ namespace BudgeIt
 
         private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
         {
-            Word.Cell cell = this.table[1].Cell(1, 1);
 
-            cell.Range.Text = "Mon";
-            cell.Range.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphRight;
         }
     }
 }
