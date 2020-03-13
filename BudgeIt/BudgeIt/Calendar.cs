6d115890d5011647905af03d752e424a8b1da3b9
Giovanni Doria <gdoria56@gmail.com>
2020-03-13 16:51:48 -0500

    Something

--- BudgeIt/BudgeIt/Calendar.cs
+++ BudgeIt/BudgeIt/Calendar.cs
@@ -4,37 +4,39 @@ using System.Windows.Forms;
 
 namespace BudgeIt
 {
-    public partial class Calendar : Form
+    private void CreateCustomCalendar()
     {
-        public SqlConnection sqlConnection = new SqlConnection();
-
-        public Calendar()
+        const string newCalendarName = "PersonalCalendar";
+        Outlook.MAPIFolder primaryCalendar = (Outlook.MAPIFolder)
+            this.Application.ActiveExplorer().Session.GetDefaultFolder
+             (Outlook.OlDefaultFolders.olFolderCalendar);
+        bool needFolder = true;
+        foreach (Outlook.MAPIFolder personalCalendar
+            in primaryCalendar.Folders)
         {
-            InitializeComponent();
-        }
-
-        private void Calendar_Load(object sender, EventArgs e)
-        {
-            try
+            if (personalCalendar.Name == newCalendarName)
             {
-                   
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
+                needFolder = false;
+                break;
             }
         }
-
-        private void btnDeposit_Click(object sender, EventArgs e)
+        if (needFolder)
         {
-            Deposit depo = new Deposit();
-            depo.sqlConnection1.ConnectionString = sqlConnection.ConnectionString;
-            depo.ShowDialog();
+            Outlook.MAPIFolder personalCalendar = primaryCalendar
+                .Folders.Add(newCalendarName,
+                    Outlook.OlDefaultFolders.olFolderCalendar);
+            Outlook.AppointmentItem newEvent =
+                personalCalendar.Items.Add
+                (Outlook.OlItemType.olAppointmentItem)
+                as Outlook.AppointmentItem;
+            newEvent.Start = DateTime.Now.AddHours(1);
+            newEvent.End = DateTime.Now.AddHours(1.25);
+            newEvent.Subject = "New plan";
+            newEvent.Body = " Meet to discuss new plan.";
+            newEvent.Save();
         }
-
-        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
+        Application.ActiveExplorer().SelectFolder(primaryCalendar
+            .Folders[newCalendarName]);
+        Application.ActiveExplorer().CurrentFolder.Display();
     }
 }
