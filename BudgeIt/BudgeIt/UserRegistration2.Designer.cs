8da739ebc065191f158920fb671ba307ac5ef9de
Niles MacKinnon <nilesnm@hotmail.com>
2020-03-14 14:07:33 -0500

    nigger

--- BudgeIt/BudgeIt/UserRegistration2.Designer.cs
+++ BudgeIt/BudgeIt/UserRegistration2.Designer.cs
@@ -39,11 +39,13 @@
             this.password = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.balance = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnBack = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnSubmit
             // 
-            this.btnSubmit.Location = new System.Drawing.Point(557, 365);
+            this.btnSubmit.Location = new System.Drawing.Point(289, 331);
             this.btnSubmit.Name = "btnSubmit";
             this.btnSubmit.Size = new System.Drawing.Size(134, 58);
             this.btnSubmit.TabIndex = 0;
@@ -54,7 +56,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(242, 72);
+            this.label1.Location = new System.Drawing.Point(187, 76);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(86, 20);
             this.label1.TabIndex = 1;
@@ -64,7 +66,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(242, 113);
+            this.label2.Location = new System.Drawing.Point(187, 117);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(86, 20);
             this.label2.TabIndex = 2;
@@ -73,7 +75,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(243, 164);
+            this.label3.Location = new System.Drawing.Point(188, 168);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(85, 20);
             this.label3.TabIndex = 3;
@@ -83,7 +85,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(243, 215);
+            this.label4.Location = new System.Drawing.Point(188, 219);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(78, 20);
             this.label4.TabIndex = 4;
@@ -91,7 +93,7 @@
             // 
             // firstName
             // 
-            this.firstName.Location = new System.Drawing.Point(372, 72);
+            this.firstName.Location = new System.Drawing.Point(317, 76);
             this.firstName.Name = "firstName";
             this.firstName.Size = new System.Drawing.Size(271, 26);
             this.firstName.TabIndex = 5;
@@ -99,29 +101,30 @@
             // 
             // lastName
             // 
-            this.lastName.Location = new System.Drawing.Point(372, 113);
+            this.lastName.Location = new System.Drawing.Point(317, 117);
             this.lastName.Name = "lastName";
             this.lastName.Size = new System.Drawing.Size(271, 26);
             this.lastName.TabIndex = 6;
             // 
             // username
             // 
-            this.username.Location = new System.Drawing.Point(372, 164);
+            this.username.Location = new System.Drawing.Point(317, 168);
             this.username.Name = "username";
             this.username.Size = new System.Drawing.Size(271, 26);
             this.username.TabIndex = 7;
             // 
             // password
             // 
-            this.password.Location = new System.Drawing.Point(372, 209);
+            this.password.Location = new System.Drawing.Point(317, 213);
             this.password.Name = "password";
             this.password.Size = new System.Drawing.Size(271, 26);
             this.password.TabIndex = 8;
+            this.password.TextChanged += new System.EventHandler(this.password_TextChanged);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(247, 269);
+            this.label5.Location = new System.Drawing.Point(192, 273);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(67, 20);
             this.label5.TabIndex = 9;
@@ -129,16 +132,37 @@
             // 
             // balance
             // 
-            this.balance.Location = new System.Drawing.Point(372, 269);
+            this.balance.Location = new System.Drawing.Point(317, 273);
             this.balance.Name = "balance";
             this.balance.Size = new System.Drawing.Size(271, 26);
             this.balance.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(374, 26);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(163, 20);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "New User Information";
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(454, 331);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(134, 58);
+            this.btnBack.TabIndex = 12;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.button1_Click);
             // 
             // UserRegistration2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1407, 519);
+            this.ClientSize = new System.Drawing.Size(867, 519);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.balance);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.password);
@@ -170,5 +194,7 @@
         private System.Windows.Forms.TextBox password;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox balance;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnBack;
     }
 }
