b3fb268e3dd0ad3c6656775838229a0b7a7496cf
malsirhani <60944732+malsirhani@users.noreply.github.com>
2020-03-14 15:59:22 -0500

    Add files via upload

---
+++ UserTransactions.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace MoneySys
+{
+    partial class UserTransactions
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBoxTransactionsUserID = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.buttonGoToWithdraw = new System.Windows.Forms.Button();
+            this.buttonGoToDeposit = new System.Windows.Forms.Button();
+            this.buttonHome = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxTransactionsCurrentBalance = new System.Windows.Forms.TextBox();
+            this.sqlConnection1 = new System.Data.SqlClient.SqlConnection();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Tahoma", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Blue;
+            this.label5.Location = new System.Drawing.Point(390, 81);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(249, 35);
+            this.label5.TabIndex = 26;
+            this.label5.Text = "All Transactions";
+            // 
+            // comboBoxTransactionsUserID
+            // 
+            this.comboBoxTransactionsUserID.FormattingEnabled = true;
+            this.comboBoxTransactionsUserID.Location = new System.Drawing.Point(396, 156);
+            this.comboBoxTransactionsUserID.Name = "comboBoxTransactionsUserID";
+            this.comboBoxTransactionsUserID.Size = new System.Drawing.Size(254, 21);
+            this.comboBoxTransactionsUserID.TabIndex = 28;
+            this.comboBoxTransactionsUserID.SelectedIndexChanged += new System.EventHandler(this.comboBoxWithdrawUserID_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(265, 156);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 19);
+            this.label4.TabIndex = 27;
+            this.label4.Text = "User ID :";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(247, 234);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(534, 295);
+            this.dataGridView1.TabIndex = 29;
+            // 
+            // buttonGoToWithdraw
+            // 
+            this.buttonGoToWithdraw.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonGoToWithdraw.ForeColor = System.Drawing.Color.Red;
+            this.buttonGoToWithdraw.Location = new System.Drawing.Point(614, 559);
+            this.buttonGoToWithdraw.Name = "buttonGoToWithdraw";
+            this.buttonGoToWithdraw.Size = new System.Drawing.Size(167, 32);
+            this.buttonGoToWithdraw.TabIndex = 30;
+            this.buttonGoToWithdraw.Text = "Go to Withdraw";
+            this.buttonGoToWithdraw.UseVisualStyleBackColor = true;
+            this.buttonGoToWithdraw.Click += new System.EventHandler(this.buttonGoToWithdraw_Click);
+            // 
+            // buttonGoToDeposit
+            // 
+            this.buttonGoToDeposit.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonGoToDeposit.ForeColor = System.Drawing.Color.Green;
+            this.buttonGoToDeposit.Location = new System.Drawing.Point(247, 559);
+            this.buttonGoToDeposit.Name = "buttonGoToDeposit";
+            this.buttonGoToDeposit.Size = new System.Drawing.Size(167, 32);
+            this.buttonGoToDeposit.TabIndex = 31;
+            this.buttonGoToDeposit.Text = "Go to Deposit";
+            this.buttonGoToDeposit.UseVisualStyleBackColor = true;
+            this.buttonGoToDeposit.Click += new System.EventHandler(this.buttonGoToDeposit_Click);
+            // 
+            // buttonHome
+            // 
+            this.buttonHome.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold);
+            this.buttonHome.Location = new System.Drawing.Point(432, 558);
+            this.buttonHome.Name = "buttonHome";
+            this.buttonHome.Size = new System.Drawing.Size(167, 33);
+            this.buttonHome.TabIndex = 32;
+            this.buttonHome.Text = "Home";
+            this.buttonHome.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(192, 191);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 19);
+            this.label1.TabIndex = 34;
+            this.label1.Text = "Current balance is :";
+            // 
+            // textBoxTransactionsCurrentBalance
+            // 
+            this.textBoxTransactionsCurrentBalance.Location = new System.Drawing.Point(396, 193);
+            this.textBoxTransactionsCurrentBalance.Name = "textBoxTransactionsCurrentBalance";
+            this.textBoxTransactionsCurrentBalance.Size = new System.Drawing.Size(254, 20);
+            this.textBoxTransactionsCurrentBalance.TabIndex = 33;
+            this.textBoxTransactionsCurrentBalance.TextChanged += new System.EventHandler(this.textBoxTransactionsCurrentBalance_TextChanged);
+            // 
+            // sqlConnection1
+            // 
+            this.sqlConnection1.ConnectionString = "Data Source=KFSC-PC;Initial Catalog=TestMoney;Integrated Security=True";
+            this.sqlConnection1.FireInfoMessageEventOnUserErrors = false;
+            // 
+            // UserTransactions
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1028, 667);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxTransactionsCurrentBalance);
+            this.Controls.Add(this.buttonHome);
+            this.Controls.Add(this.buttonGoToDeposit);
+            this.Controls.Add(this.buttonGoToWithdraw);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboBoxTransactionsUserID);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Name = "UserTransactions";
+            this.Text = "UserTransactions";
+            this.Load += new System.EventHandler(this.UserTransactions_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBoxTransactionsUserID;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button buttonGoToWithdraw;
+        private System.Windows.Forms.Button buttonGoToDeposit;
+        private System.Windows.Forms.Button buttonHome;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxTransactionsCurrentBalance;
+        private System.Data.SqlClient.SqlConnection sqlConnection1;
+    }
+}
