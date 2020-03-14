2b549baf7f39c1ac03c2212f74bc7d26fa11cad9
malsirhani <60944732+malsirhani@users.noreply.github.com>
2020-03-14 16:13:31 -0500

    Add files via upload

--- BudgeIt/BudgeIt/Program.cs
+++ BudgeIt/BudgeIt/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace BudgeIt
+namespace MoneySys
 {
     static class Program
     {
@@ -16,7 +16,7 @@ namespace BudgeIt
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login());
+            Application.Run(new Deposit());
         }
     }
 }
